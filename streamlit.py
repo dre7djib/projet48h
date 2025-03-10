af2e7ce9195a1507c02c515cca42fa9d12043d04
dre7djib <djibril.camara.pradier@gmail.com>
2025-03-10 17:07:16 +0100

    Tags Streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,29 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+#pip3 install openpyxl  
 
 uploaded_file = "filtered_tweets_engie_cleaned.xlsx"
 df = pd.read_excel(uploaded_file)
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+    options = ["Problèmes de facturation", "Pannes et urgences", "Service client injoignable", "Problèmes avec l’application", "Délai d’intervention"]
+    selection = st.pills("Catégories", options, selection_mode="multi")
 
-st.title('Dashboard Projet 48h')
+if "Problèmes de facturation" in selection or selection == []:
+    st.title('Dashboard Projet 48h')
+    st.write('## Données')
+    st.dataframe(df.head())  
 
-st.write('## Données')
-st.dataframe(df.head())  
+    df_count = df.groupby("heure").size().reset_index(name='count')
 
-df_count = df.groupby("heure").size().reset_index(name='count')
-
-st.write('## Nombre de tweets par heure')
-st.bar_chart(df_count.set_index('heure'))
+    st.write('## Nombre de tweets par heure')
+    st.bar_chart(df_count.set_index('heure'))
